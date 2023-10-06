3b8fe86a206ee98d0ccb0ae77713fceef0b684ff
saifsammy <saifsammy653@gmail.com>
2023-10-06 15:37:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">
+        Hello world! This is A demo
+      </h1>
+    </>
+  )
+}
+
+export default App
